64530d42f55affcc89cb269121f310e72005ec76
Daniel Sun <dsun@actra.ca>
2026-02-12 14:01:57 -0800

    change

--- obj/Debug/net10.0/StudentFunctions.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentFunctions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentFunctions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ecb4b910c67242c035f377a5c220c83534f6653")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentFunctions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentFunctions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
